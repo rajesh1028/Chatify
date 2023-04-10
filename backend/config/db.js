d3c6794cc23ee03e1e4fbd9ec38ff3a244309e47
Krishna-7777 <kchhabraji13@gmail.com>
2023-04-10 11:44:57 +0530

    Added Comments

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// Connecting To the MongoDB present on MongoDB Atlas
 const connection=mongoose.connect(process.env.mongoURL);
 
 module.exports={
--- backend/google-auth.js
+++ backend/google-auth.js
@@ -4,18 +4,21 @@ const jwt = require("jsonwebtoken");
 const { UserModel } = require('./models/user.model');
 require("dotenv").config();
 
-
+// Configure passport to use Google OAuth strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://127.0.0.1:3000/auth/google/callback"
   },
   async function(accessToken, refreshToken, profile, cb) {
+    // Check if user already exists in the database
     let euser=await UserModel.findOne({email:profile.emails[0].value});
     if(euser){
+      // If user exists, generate a JWT token and add it to the profile object
       let token=jwt.sign({"_id":euser._id},process.env.secret);
       profile["t"]=token;
     }else{
+      // If user does not exist, create a new user in the database with email and given name, then generate a JWT token and add it to the profile object
       let nuser= new UserModel({email:profile.emails[0].value,name:profile.name.givenName})
       await nuser.save();
       euser=await UserModel.findOne({email:profile.emails[0].value});
@@ -23,6 +26,7 @@ passport.use(new GoogleStrategy({
       profile["t"]=token;
     }
     
+    // Export the profile object
     module.exports={
       profile
     }
@@ -34,5 +38,5 @@ passport.use(new GoogleStrategy({
   }
 ));
   
-
+// Export the passport object for use in other modules
 module.exports={passport}
